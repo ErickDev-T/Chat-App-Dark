7014791de1d2fe98a231a40f15f92d2de44cff87
Erick Tejada <erickjtm2222@gmail.com>
2024-10-04 10:26:50 -0400

    Consumiendo API y mejorando img

--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -16,7 +16,7 @@ class MyMessageBubble extends StatelessWidget {
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              'se supone que aqui va un msg',
+              'incididunt qui excepteur elit.',
               style: TextStyle(color: Colors.white),
             ),
           ),
