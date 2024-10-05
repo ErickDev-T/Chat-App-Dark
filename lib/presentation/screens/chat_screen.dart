f7f1b9a659d4ea6424db7193e7613cbd77bddcc2
Erick Tejada <erickjtm2222@gmail.com>
2024-10-05 16:13:19 -0400

    init

--- lib/presentation/screens/chat_screen.dart
+++ lib/presentation/screens/chat_screen.dart
@@ -43,7 +43,6 @@ class _Chatview extends StatelessWidget {
                 },
               ),
             ),
-
             //caja de texto de msg
             const MessageFieldBox(),
           ],
--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -16,7 +16,7 @@ class HerMessageBubble extends StatelessWidget {
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              'ssssssssssssssssssslit.',
+              'Veniam minim fugiat llit cupidatat laborum irure ex.',
               style: TextStyle(color: Colors.white),
             ),
           ),
